39351e42bd2e60e60aedc7e97ad98ea9159992f4
runaydhaygude <runaydhaygude97@gmail.com>
2025-01-04 22:33:04 +0530

    Add codebuild / codedeploy spec

--- infra/deploy-infra.sh
+++ infra/deploy-infra.sh
@@ -8,6 +8,23 @@ EC2_INSTANCE_TYPE=t2.micro
 
 echo -e "EC2_INSTANCE_TYPE: $EC2_INSTANCE_TYPE" 
 
+AWS_ACCOUNT_ID=aws sts get-caller-identity --profile awsbootstrap  --query "Account" --output text
+ 
+CODEPIPELINE_BUCKET="$STACK_NAME-$REGION-codepipeline-$AWS_ACCOUNT_ID"
+
+
+# Deploys static resources
+echo -e "\n\n=========== Deploying setup.yml ==========="
+
+aws cloudformation deploy \
+    --region $REGION \
+    --profile $CLI_PROFILE \
+    --stack-name $STACK_NAME-setup \
+    --template-file setup.yml \
+    --no-fail-on-empty-changeset \
+    --parameter-overrides CodePipelineBucket=$CODEPIPELINE_BUCKET
+
+
 # Deploy the CloudFormation template
 echo -e "\n\n=========== Deploying main.yml ==========="
 aws cloudformation deploy \
